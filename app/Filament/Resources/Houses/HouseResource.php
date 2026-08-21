8c514c62910f2f814b00bef715e2b6d4e8f0a810
Syahyudi <syahyudi660@gmail.com>
2026-08-21 11:30:49 +0700

    CRUD House 1

--- app/Filament/Resources/Houses/HouseResource.php
+++ app/Filament/Resources/Houses/HouseResource.php
@@ -17,8 +17,11 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Repeater;
 use Filament\Schemas\Components\Fieldset;
 use Filament\Forms\Components\Select;
+use App\Models\Facility;
+use Filament\Forms\Components\Textarea;
 
 class HouseResource extends Resource
 {
@@ -48,7 +51,7 @@ class HouseResource extends Resource
                                 'SHGB' => 'SHGB',
                             ]),
 
-                        FileUpload::make('thumnail')
+                        FileUpload::make('thumbnail')
                             ->required()
                             ->image()
                             ->acceptedFileTypes([
@@ -56,8 +59,80 @@ class HouseResource extends Resource
                                 'image/png',
                                 'image/webp',
 
-                            ])
-                    ])
+                            ]),
+
+                        Repeater::make('photos')
+                            ->relationship('photos')
+                            ->schema([
+                                FileUpload::make('photo')
+                                    ->required(),
+                            ]),
+
+                        Repeater::make('facilities')
+                            ->relationship('facilities')
+                            ->schema([
+                                Select::make('facility_id')
+                                    ->label('Facility')
+                                    ->options(Facility::all()->pluck('name', 'id'))
+                                    ->searchable()
+                                    ->required(),
+                            ]),
+                    ]),
+                Fieldset::make('Additional')
+                    ->columnSpanFull()
+                    ->schema([
+                        Select::make('city_id')
+                            ->relationship('city', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Textarea::make('about')
+                            ->required(),
+
+                        TextInput::make('electric')
+                            ->required()
+                            ->numeric()
+                            ->prefix('Watts'),
+
+                        TextInput::make('land_area')
+                            ->required()
+                            ->numeric()
+                            ->prefix('m²'),
+
+                        TextInput::make('building_area')
+                            ->required()
+                            ->numeric()
+                            ->prefix('m²'),
+
+                        TextInput::make('bedroom')
+                            ->required()
+                            ->numeric()
+                            ->prefix('Unit'),
+
+                        TextInput::make('bathroom')
+                            ->required()
+                            ->numeric()
+                            ->prefix('Unit'),
+
+                        Select::make('city_id')
+                            ->relationship('city', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+
+                        Select::make('category_id')
+                            ->relationship('category', 'name')
+                            ->searchable()
+                            ->preload()
+                            ->required(),
+                    ]),
             ]);
     }
 
