82c9e8dd328efcdc4b7b81956c3945859c587b2b
Syahyudi <syahyudi660@gmail.com>
2026-08-30 17:01:51 +0700

    CRUD request mortgage part 1

--- app/Filament/Resources/MortgageRequests/MortgageRequestResource.php
+++ app/Filament/Resources/MortgageRequests/MortgageRequestResource.php
@@ -7,14 +7,19 @@ use App\Filament\Resources\MortgageRequests\Pages\EditMortgageRequest;
 use App\Filament\Resources\MortgageRequests\Pages\ListMortgageRequests;
 use App\Filament\Resources\MortgageRequests\Schemas\MortgageRequestForm;
 use App\Filament\Resources\MortgageRequests\Tables\MortgageRequestsTable;
+use App\Models\Interest;
 use App\Models\MortgageRequest;
 use BackedEnum;
+use Filament\Forms\Components\Select;
 use Filament\Resources\Resource;
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Components\Wizard\Step;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Schemas\Components\Grid;
 
 class MortgageRequestResource extends Resource
 {
@@ -24,9 +29,58 @@ class MortgageRequestResource extends Resource
 
     public static function form(Schema $schema): Schema
     {
-        return MortgageRequestForm::configure($schema);
-    }
+        return $schema
+            ->components([
+                Wizard::make([
+                    Step::make('Product And Price')
+                        ->schema([
+                            Grid::make(3)
+                                ->schema([ // Perbaikan 1: Grid membungkus isinya dengan schema([])
+                                    Select::make('house_id')
+                                        ->label('House') // Perbaikan 2: >label menjadi ->label
+                                        ->options(\App\Models\House::query()->pluck('name', 'id'))
+                                        ->searchable()
+                                        ->preload()
+                                        ->required()
+                                        ->live()
+                                        ->afterStateUpdated(function ($state, callable $set) {
+                                            $house = \App\Models\House::find($state);
+                                            if ($house) {
+                                                $set('house_price', $house->price ?? 0);
+                                            }
+                                        }),
 
+                                    Select::make('interest_id')
+                                        ->label('Interest in %')
+                                        ->options(function (callable $get) {
+                                            $houseId = $get('house_id');
+                                            if ($houseId) {
+                                                return Interest::where('house_id', $houseId)
+                                                    ->get()
+                                                    ->pluck('interest', 'id');
+                                            }
+                                            return [];
+                                        })
+                                        ->preload()
+                                        ->required()
+                                        ->live()
+                                        ->afterStateUpdated(function ($state, callable $set) {
+                                            $interest = \App\Models\Interest::find($state);
+                                            if ($interest) {
+                                                $set('bank_name', $interest->bank->name ?? '');
+                                                $set('interest', $interest->interest);
+                                                $set('duration', $interest->duration);
+                                            }
+                                        }),
+
+                                ])
+                        ])
+                ])
+                    ->columnSpan('full')
+                    ->columns(1)
+                    ->skippable()
+            ]);
+    }
     public static function table(Table $table): Table
     {
         return MortgageRequestsTable::configure($table);
