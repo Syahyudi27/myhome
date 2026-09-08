ef77319c6721f0fe1c326f652edf694821e16b40
syahyudi27 <syahyudi660@gmail.com>
2026-09-08 20:52:19 +0700

    House Service

---
+++ app/Services/HouseService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\Category;
+use App\Models\City;
+use App\Models\House;
+
+class HouseService
+{
+    public function getCategoriesAndCities()
+    {
+        return [
+            'categories' => Category::latest()->get(),
+            'city' => City::latest()->get(),
+        ];
+    }
+
+    public function searchHouses($filters)
+    {
+        $query = House::query();
+
+        if (!empty($filters['city'])) {
+            $query->where('city_id', $filters['city']);
+        }
+
+        if (!empty($filters['category'])) {
+            $query->where('category_id', $filters['category']);
+        }
+
+        $houses = $query->get();
+
+        $category = Category::findOrFail($filters['category'] ?? null);
+        $city = City::findOrFail($filters['city'] ?? null);
+
+        return compact('houses', 'category', 'city');
+    }
+}
